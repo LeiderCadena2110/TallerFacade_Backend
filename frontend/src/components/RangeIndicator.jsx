968ce90694b8f4840821d61d00c8a4f7321d417f
Diego Armando Pinta Cuasquen <diarpicu2022@gmail.com>
2026-04-10 17:53:27 -0500

    Actualización del proyecto backend

--- frontend/src/components/RangeIndicator.jsx
+++
@@ -1,7 +0,0 @@
-export default function RangeIndicator({ dentroRango }) {
-  return (
-    <span className={`range-indicator ${dentroRango ? 'in-range' : 'out-of-range'}`}>
-      {dentroRango ? '✓' : '✗'}
-    </span>
-  )
-}
